53bf5f7a99487d283bf613734080efb215eaf421
postonoh <postonoh@att.net>
2020-08-12 14:16:10 -0400

    Code adjustment

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,37 +1,36 @@
-import React from 'react';
-import './Sidebar.css';
+import React from "react";
+import "./Sidebar.css";
 import SideBarOption from "./SideBarOption";
-import TwitterIcon from '@material-ui/icons/Twitter';
-import HomeIcon from '@material-ui/icons/Home';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import SearchIcon from '@material-ui/icons/Search';
-import MailOutlineIcon from '@material-ui/icons/MailOutline';
-import BookmarkIcon from '@material-ui/icons/Bookmark';
-import ListAltIcon from '@material-ui/icons/ListAlt';
-import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import TwitterIcon from "@material-ui/icons/Twitter";
+import HomeIcon from "@material-ui/icons/Home";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import SearchIcon from "@material-ui/icons/Search";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkIcon from "@material-ui/icons/Bookmark";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import PersonOutlineIcon from "@material-ui/icons/PersonOutline";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import { Button } from "@material-ui/core";
 
-
 function Sidebar() {
-    return (
-        <div className="sidebar">
-          <TwitterIcon className="sidebar__twitterIcon"/>
+  return (
+    <div className="sidebar">
+      <TwitterIcon className="sidebar__twitterIcon" />
 
+      <SideBarOption active Icon={HomeIcon} text="Home" />
+      <SideBarOption Icon={SearchIcon} text="Explore" />
+      <SideBarOption Icon={NotificationsIcon} text="Notification" />
+      <SideBarOption Icon={MailOutlineIcon} text="Message" />
+      <SideBarOption Icon={BookmarkIcon} text="Bookmarks" />
+      <SideBarOption Icon={ListAltIcon} text="Lists" />
+      <SideBarOption Icon={PersonOutlineIcon} text="Profile" />
+      <SideBarOption Icon={MoreHorizIcon} text="More" />
 
-            <SideBarOption active Icon={HomeIcon} text="Home" />
-            <SideBarOption Icon={SearchIcon} text="Explore" />
-            <SideBarOption Icon={NotificationsIcon} text="Notification" />
-            <SideBarOption Icon={MailOutlineIcon} text="Message" />
-            <SideBarOption Icon={BookmarkIcon} text="Bookmarks" />
-            <SideBarOption Icon={ListAltIcon} text="Lists" />
-            <SideBarOption Icon={PersonOutlineIcon} text="Profile" />
-            <SideBarOption Icon={MoreHorizIcon} text="More" />
-
-            <Button variant="outlined" className="sidebar__tweet" fullWidth >Tweet</Button>
-
-        </div>
-    )
+      <Button variant="outlined" className="sidebar__tweet" fullWidth>
+        Tweet
+      </Button>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
