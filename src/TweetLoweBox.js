fce38061c2d8605c6376c4197704ba652c8e5439
postonoh <postonoh@att.net>
2020-08-17 01:15:01 -0400

    Working on the Gif like twitter

--- src/TweetLoweBox.js
+++ src/TweetLoweBox.js
@@ -1,10 +1,10 @@
 import React, { useState, useRef } from "react";
-import ImageIcon from "@material-ui/icons/Image"
+import ImageIcon from "@material-ui/icons/Image";
 import GifIcon from "@material-ui/icons/Gif";
 import SentimentSatisfiedSharpIcon from "@material-ui/icons/SentimentSatisfiedSharp";
 import ScheduleRoundedIcon from "@material-ui/icons/ScheduleRounded";
 import { Button } from "@material-ui/core";
-import { Grid } from "@giphy/react-components";
+import { Grid, Gif } from "@giphy/react-components";
 import { GiphyFetch } from "@giphy/js-fetch-api";
 import { withStyles } from "@material-ui/core/styles";
 import Dialog from "@material-ui/core/Dialog";
@@ -15,6 +15,7 @@ import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 import Typography from "@material-ui/core/Typography";
 import "./TweetLowerBox.css";
+import SearchIcon from "@material-ui/icons/Search";
 
 const styles = (theme) => ({
   root: {
@@ -63,7 +64,7 @@ const DialogActions = withStyles((theme) => ({
 function TweetLoweBox() {
   const uploadedImage = useRef(null);
   const imageUploader = useRef(null);
-  const [tweetGifs, setweetGifs] = useState("");
+  const [tweetGifs, settweetGifs] = useState("");
   const [open, setOpen] = useState(false);
 
   const handleOpen = () => {
@@ -71,35 +72,43 @@ function TweetLoweBox() {
   };
   const handleClose = () => {
     setOpen(false);
-  }
-  
-  const handleImageUpload = e => {
+  };
+
+  const handleImageUpload = (e) => {
     const [file] = e.target.files;
     if (file) {
       const reader = new FileReader();
       const { current } = uploadedImage;
       current.file = file;
-      reader.onload = e => {
+      reader.onload = (e) => {
         current.src = e.target.result;
       };
       reader.readAsDataURL(file);
     }
   };
 
+  const gf = new GiphyFetch("zOGPG3EwagQMXW2vEwWv0zvaGvmFJU2x");
+  const fetchGifs = (offset: number) => gf.trending({ offset, limit: 10 });
+  const categories= gf.categories();
  
+
+
   return (
-    <div>      
-      <div className="gifPopup__boxFooter"> 
-       <input
-        type="file"
-        accept="image/*"
-        onChange={handleImageUpload}
-        ref={imageUploader}
-        style={{
-          display: "none"
-        }}
-      />
-      <ImageIcon  onClick={() => imageUploader.current.click()}>/></ImageIcon>
+    <div>
+      <div className="gifPopup__boxFooter">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageUpload}
+          ref={imageUploader}
+          style={{
+            display: "none",
+          }}
+        />
+        <ImageIcon
+          onClick={() => imageUploader.current.click()}
+          ref={uploadedImage}
+        />
         <GifIcon className="gifPopup_gifIcon" onClick={handleOpen} />
         <SentimentSatisfiedSharpIcon className="" tweet__sentiicon />
         <ScheduleRoundedIcon className="gifPopup__scheduleIcon" />
@@ -108,40 +117,32 @@ function TweetLoweBox() {
           aria-labelledby="customized-dialog-title"
           open={open}
         >
-          <DialogTitle id="customized-dialog-title" onClose={handleClose}>
-            <div>
+          <div className="animatedgifContainer">
+            <DialogTitle
+              id="customized-dialog-title"
+              onClose={handleClose}
+            ></DialogTitle>
+            <div className="gif__searchContainer">
+              <SearchIcon className="gif__searchIcon" />
               <input
-                onChange={(e) => setweetGifs(e.target.value)}
+                onChange={(e) => settweetGifs(e.target.value)}
                 className="gifPopup__gifSearch"
                 placeholder="Search for Gifs"
                 type="text"
-                value={setweetGifs}
+                value={settweetGifs}
               />
             </div>
-          </DialogTitle>
+          </div>
           <DialogContent dividers>
             <Typography gutterBottom>
-              Next to add gif from https://giphy.com/
-            </Typography>
-            <Typography gutterBottom>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur
-              et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
-              auctor.
-            </Typography>
-            <Typography gutterBottom>
-              Aenean lacinia bibendum nulla sed consectetur. Praesent commodo
-              cursus magna, vel scelerisque nisl consectetur et. Donec sed odio
-              dui. Donec ullamcorper nulla non metus auctor fringilla.
-            </Typography>
+              <div>
+              <Grid width={500} columns={3} gutter={6} fetchGifs={fetchGifs} />
+              </div>
+            </Typography>          
           </DialogContent>
-          <DialogActions>
-            <Button autoFocus onClick={handleClose} color="primary">
-              Save changes
-            </Button>
-          </DialogActions>
         </Dialog>
-        </div>
       </div>
+    </div>
   );
 }
 
